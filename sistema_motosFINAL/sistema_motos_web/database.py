01907fc454a7d40d587beb0f8d8d6ac7fa4c6ef1
Gabriel Eufrazio <gabrieldomingues13@hotmail.com>
2025-09-02 15:59:31 -0300

    Ajuste porque sobrescreveu

--- sistema_motosFINAL/sistema_motos_web/database.py
+++ sistema_motosFINAL/sistema_motos_web/database.py
@@ -500,6 +500,7 @@ def gerar_pdf_garantia(moto_id, venda_id=None):
     c.drawString(margem_esq, y, f"{str(nome_cliente or '')}")
     y -= 14
     c.drawString(margem_esq + 260, y, "1000GIROSMOTOS")
+    y -= 14
     c.drawString(margem_esq + 260, y, f"CNPJ {vendedor_cnpj}")
     c.save()
 
